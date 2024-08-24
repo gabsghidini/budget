bdb7de43b41e9cf27b5edd186a9e2ab330eae689
gabsghidini <gabsghidini@gmail.com>
2024-08-24 12:15:39 -0300

    FEAT: Dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardPage = () => {
+	return (
+		<div>
+			<h1>Dashboard</h1>
+			{/* Adicione os componentes e conteúdo do seu dashboard aqui */}
+		</div>
+	);
+};
+
+export default DashboardPage;

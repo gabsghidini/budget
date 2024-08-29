137368bb0de8a48f6e9b512d232982f45c29b5fe
gabsghidini <gabriella.ferreira@xnetweb.com.br>
2024-08-29 20:19:16 -0300

    feat: moving login to root

---
+++ app/components/financialSummary.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./styles";
+
+export const FinancialSummary = () => {
+	return (
+		<div>
+			<nav>
+				<h2>Resumo Financeiro</h2>
+				<button>Todos</button>
+				<button>Entradas</button>
+				<button>Saídas</button>
+			</nav>
+		</div>
+	);
+};
